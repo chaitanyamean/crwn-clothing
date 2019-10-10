73e83038a9d0f9b49d5eecd49e0186082c053bfc
Krishna chaitanya Vulchi <chaitanyamean@gmail.com>
2019-10-10 15:15:58 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
 
+import { Route, Redirect } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => {
+
+  return (
+
+    <div>
+      <h1>
+        This is hats page
+    </h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop/hats' component={HatsPage} />
+      <Redirect from='*' to='/' />
     </div>
   );
 }
